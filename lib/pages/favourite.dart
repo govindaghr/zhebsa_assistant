f6273d19a75c18e97d642e4a3d5583075890d26b
Govinda Ghimeray <ghimry@gmail.com>
2022-02-24 23:54:58 +0600

    Added ListView Builder to favourite page

--- lib/pages/favourite.dart
+++ lib/pages/favourite.dart
@@ -3,17 +3,33 @@ import 'package:flutter/material.dart';
 class FavouritePage extends StatelessWidget {
   const FavouritePage({Key? key}) : super(key: key);
 
+//del later and replace with real data
+  FavouritePage? get product => null;
+  Object? get name => null;
+  void selectItem(FavouritePage product) {
+    print(product.name);
+  }
+
   @override
   Widget build(BuildContext context) {
     // setPageTitle('Favourite Page', context);
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        transformAlignment: Alignment.topCenter,
-        child: const Center(
-          child: Text('FavouritePage Screen'),
-        ),
+      body: ListView.builder(
+        itemCount: 15,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            elevation: 2,
+            child: ListTile(
+              // leading: FlutterLogo(size: 56.0),
+              onTap: () => selectItem(product!),
+              leading: Icon(Icons.list),
+              trailing: Icon(Icons.more_vert),
+              title: Text('Two-line ListTile'),
+              subtitle: Text('Here is a second line'),
+            ),
+          );
+        },
+        padding: const EdgeInsets.all(8),
       ),
     );
   }
