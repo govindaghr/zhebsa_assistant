9bcd3af5dd4757112cb0fa5fb3d8d473489a270e
Govinda Ghimeray <ghimry@gmail.com>
2022-03-11 16:54:35 +0600

    sizer package and media query is replaced by responsive_framework

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'pages/home.dart';
 import 'locale/locale_string.dart';
 import 'package:get/get.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() => runApp(const ZhebsaApp());
 
@@ -13,14 +15,22 @@ class ZhebsaApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       /*Media Queary scale factor to maintain same size across different media */
-      builder: (context, child) {
-        return MediaQuery(
-          child: Container(
-            child: child,
-          ),
-          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-        );
-      },
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, widget!),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(450, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+        background: Container(
+          color: const Color(0xFFF5F5F5),
+        ),
+      ),
       title: 'Zhebsa Assistant',
       debugShowCheckedModeBanner: false,
       translations: LocaleString(),
